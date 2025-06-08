19cdec75ef959012187d4cb8fde34b890e899860
Raygell <me@rmjtromp.com>
2025-06-09 00:45:55 +0200

    Add Javadoc comment for hasBeenCalled method and remove unused Result enum

--- src/main/java/com/rmjtromp/events/Event.java
+++ src/main/java/com/rmjtromp/events/Event.java
@@ -87,28 +87,16 @@ public abstract class Event {
         return async;
     }
 
+    /**
+     * Indicates whether the event has been marked as "called." This typically
+     * signifies that the event has been dispatched to its registered handlers
+     * using the event system.
+     *
+     * @return true if the event has been marked as called; false otherwise
+     */
+    @Contract(pure = true)
     public boolean hasBeenCalled() {
         return called;
     }
 
-    public enum Result {
-
-        /**
-         * Deny the event. Depending on the event, the action indicated by the
-         * event will either not take place or will be reverted. Some actions
-         * may not be denied.
-         */
-        DENY,
-        /**
-         * Neither deny nor allow the event. The server will proceed with its
-         * normal handling.
-         */
-        DEFAULT,
-        /**
-         * Allow / Force the event. The action indicated by the event will
-         * take place if possible, even if the server would not normally allow
-         * the action. Some actions may not be allowed.
-         */
-        ALLOW;
-    }
 }
