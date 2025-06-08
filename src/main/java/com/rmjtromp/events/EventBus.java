abd2556d64139335fb8e160dee435688729ae894
Raygell <me@rmjtromp.com>
2025-06-09 00:44:35 +0200

    Add Javadoc comments for register and post methods in EventBus

--- src/main/java/com/rmjtromp/events/EventBus.java
+++ src/main/java/com/rmjtromp/events/EventBus.java
@@ -54,6 +54,12 @@ public final class EventBus {
         };
     }
 
+    /**
+     * Registers a listener to the event handling system. This allows the listener
+     * to receive and handle events it is subscribed to.
+     *
+     * @param listener The object containing methods annotated for handling events.
+     */
     public static void register(@NotNull Object listener) {
         for (Map.Entry<Class<? extends Event>, Set<RegisteredListener>> entry : RegisteredListener.createRegisteredListeners(listener).entrySet()) {
             Class<? extends Event> clazz = entry.getKey();
@@ -73,6 +79,14 @@ public final class EventBus {
         }
     }
 
+    /**
+     * Posts an event to all registered listeners that handle the event type.
+     * The event is marked as "called" and is passed to each listener in the order
+     * of their registration. In case a listener throws an exception while handling
+     * the event, it will be logged, and the exception will be printed to the stack trace.
+     *
+     * @param event The event to be posted to the registered listeners.
+     */
     public static void post(@NotNull Event event) {
         HandlerList handlers = event.getHandlers();
         RegisteredListener[] listeners = handlers.getRegisteredListeners();
@@ -84,11 +98,20 @@ public final class EventBus {
             } catch (Exception ex) {
                 log.warning("Could not pass event " + event.getEventName() + " to " + registration.getListener().getClass().getName());
                 log.throwing(EventBus.class.getName(), "callEvent", ex);
-                ex.printStackTrace();
             }
         }
     }
 
+    /**
+     * Retrieves or creates the {@link HandlerList} for the specified event type.
+     * This method ensures that each event type has a unique {@link HandlerList}
+     * associated with it, which manages the handlers for that particular type of event.
+     *
+     * @param type The class of the event for which the {@link HandlerList} is needed.
+     *             Must not be null.
+     * @return The {@link HandlerList} associated with the provided event type, or null
+     *         if an exception occurs during the retrieval or creation of the list.
+     */
     private static HandlerList getEventListeners(@NotNull Class<? extends Event> type) {
         try {
             return Event.getHandlersMap().computeIfAbsent(type, k -> new HandlerList());
